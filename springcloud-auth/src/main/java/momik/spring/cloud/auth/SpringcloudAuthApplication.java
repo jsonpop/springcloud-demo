509b05f07df3e0b717e425fcf4e7234f3e31d078
momik <2643053021@qq.com>
2019-07-03 22:13:56 +0800

    初始化项目

---
+++ springcloud-auth/src/main/java/momik/spring/cloud/auth/SpringcloudAuthApplication.java
@@ -0,0 +1,13 @@
+package momik.spring.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudAuthApplication.class, args);
+    }
+
+}
---
+++ springcloud-auth/src/main/java/momik/spring/cloud/auth/config/AuthorizationServerConfig.java
@@ -0,0 +1,58 @@
+package momik.spring.cloud.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@EnableAuthorizationServer
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    /**
+     * 注入authenticationManager
+     * 来支持 password grant type
+     */
+    /*@Autowired
+    private AuthenticationManager authenticationManager;*/
+
+    /*@Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()")
+                .allowFormAuthenticationForClients();
+    }*/
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("clientId-one")
+                .secret("client-secret-one")
+                .authorizedGrantTypes("password", "authorization_code", "client_credentials", "implicit", "refresh_token")
+                .redirectUris("www.baidul.com")
+                .scopes("all");
+    }
+
+    /*@Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }*/
+
+    /**
+     * 加密方式 security2.0以后 密码无法明文保存，必须要经过加密
+     *
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
